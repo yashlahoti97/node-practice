4dd82c36ee7cfa421ab9386672628457891c443f
Yash Lahoti <yashlahoti@Yashs-MacBook-Air.local>
2020-05-16 14:01:17 -0700

    outof callback hell
    Signed-off-by: Yash Lahoti <yashlahoti@Yashs-MacBook-Air.local>

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,5 +1,37 @@
 console.log('Before');
-setTimeout(()=>{
-    console.log("Reading user from db...");
-}, 2000);
+getUser(1, getRepositories );
 console.log('After');
+
+function getRepositories (user) {
+    getRepositories(user.gitHubUsername, getCommits);
+}
+
+function getCommits(repos) {
+    console.log('Repos: ',repos);
+    getCommits(repos, displayCommits);
+}
+
+function displayCommits(commits) {
+    console.log(commits);
+}
+
+function getUser(id, callback) {
+    setTimeout(()=>{
+        console.log('Reading user from db...');
+        callback({ id: id, gitHubUsername: 'yash'});
+    }, 2000);
+}
+
+function getRepositories(username, callback) {
+    setTimeout(() => {
+        console.log('Calling GitHub API...');
+        callback(['repo1','repo2','repo3']);
+    },2000);
+}
+
+function getCommits(repository, callback) {
+    setTimeout(() => {
+        console.log('Calling GitHub API...');
+        callback(['commit1','commit2','commit3']);
+    },2000);
+}
