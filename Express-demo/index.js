0c342c93ab70bfb12474dbe0dc90f7c9da6cad86
Yash Lahoti <yashlahoti@Yashs-MacBook-Air.local>
2020-05-15 23:52:58 -0700

    debugger added
    Signed-off-by: Yash Lahoti <yashlahoti@Yashs-MacBook-Air.local>

--- Express-demo/index.js
+++ Express-demo/index.js
@@ -1,8 +1,31 @@
 const Joi = require('Joi');
+const config = require('config');
+const debug = require('debug')('app:startup');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const logger = require('./logger');
 const express = require('express');
 const app = express();
 
-app.use(express.json());
+
+
+app.use(express.json()); 
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+app.use(helmet());
+
+//configuration
+console.log('Application Name: '+ config.get('name'));
+console.log('Mail Server: '+ config.get('mail.host'));
+
+console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
+console.log(`app: ${app.get('env')}`);
+
+if( process.env.NODE_ENV === 'development') {
+    app.use(morgan('tiny'));
+    debug('Morgan Enabled...');
+}
+
 const courses = [
     { id:1, name:'course1'},
     { id:2, name:'course2'},
---
+++ Express-demo/logger.js
@@ -0,0 +1,5 @@
+
+function log(req, res, next) {
+    console.log('Logging...');
+}
+module.exports = log;
