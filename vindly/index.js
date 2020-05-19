1f9c6f7f6b1129b273e2979f473bde9fb39d73fe
Yash Lahoti <yashlahoti97@gmail.com>
2020-05-18 18:31:03 -0700

    refactoring and restructuring
    Signed-off-by: Yash Lahoti <yashlahoti97@gmail.com>

--- vindly/index.js
+++ vindly/index.js
@@ -1,22 +1,11 @@
 require('express-async-errors');
-
 const express = require('express');
-const Joi = require('joi');
-const config = require('config');
-
-Joi.objectId = require('joi-objectid')(Joi);
-
 const app = express();
 
 require('./startup/routes')(app);
 require('./startup/db')();
+require('./startup/config')();
+require('./startup/validation')();
 
-if (!config.get('jwtPrivateKey')) {
-    console.error("FATAL ERROR: jwtPrivateKey not defined");
-    process.exit(1);
-}
 const port = process.env.PORT || 3000;
-
 app.listen(port, () => console.log(`Listening to port ${port}`));
-
-
