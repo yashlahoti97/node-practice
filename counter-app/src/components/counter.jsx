df4dbbfa405fa8102e506f18baca8ae131868502
Yash Lahoti <yashlahoti97@gmail.com>
2020-06-11 23:44:56 -0700

    composing components lecture14
    Signed-off-by: Yash Lahoti <yashlahoti97@gmail.com>

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,30 +1,23 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    count: 0,
-  };
-
-  constructor() {
-    super();
-    // this.handleIncrement = this.handleIncrement.bind(this);
-  }
-
-  handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
-
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={() => {
-            this.handleIncrement({ id: 1 });
-          }}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>
+        <button
+          onClick={() => {
+            this.props.onDelete(this.props.counter.id);
+          }}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
       </div>
     );
@@ -32,13 +25,13 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.props.counter;
+    return value === 0 ? "Zero" : value;
   }
 }
 
---
+++ counter-app/src/components/counters.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import Counter from "./counter";
+class Counters extends Component {
+  render() {
+    const { onReset, onDelete, onIncrement, counters } = this.props;
+    return (
+      <div>
+        <button onClick={onReset} className="btn btn-primary btn-sm m-2">
+          Reset
+        </button>
+        {counters.map((counter) => (
+          <Counter
+            key={counter.id}
+            counter={counter}
+            onIncrement={onIncrement}
+            onDelete={onDelete}
+          ></Counter>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Counters;
