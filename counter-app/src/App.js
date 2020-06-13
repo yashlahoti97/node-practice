ccb0b34565d4866259501db041fb4eeb6a9df2a6
Yash Lahoti <yashlahoti97@gmail.com>
2020-06-12 20:22:26 -0700

    composing components complete
    Signed-off-by: Yash Lahoti <yashlahoti97@gmail.com>

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -29,6 +29,14 @@ class App extends Component {
     this.setState({ counters });
   };
 
+  handleDecrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value--;
+    this.setState({ counters });
+  };
+
   handleDelete = (counterId) => {
     const counters = this.state.counters.filter((c) => c.id !== counterId);
     this.setState({ counters });
@@ -45,6 +53,7 @@ class App extends Component {
             onReset={this.handleReset}
             onIncrement={this.handleIncrement}
             onDelete={this.handleDelete}
+            onDecrement={this.handleDecrement}
           ></Counters>
         </main>
       </React.Fragment>
--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -2,7 +2,13 @@ import React, { Component } from "react";
 import Counter from "./counter";
 class Counters extends Component {
   render() {
-    const { onReset, onDelete, onIncrement, counters } = this.props;
+    const {
+      onReset,
+      onDelete,
+      onIncrement,
+      onDecrement,
+      counters,
+    } = this.props;
     return (
       <div>
         <button onClick={onReset} className="btn btn-primary btn-sm m-2">
@@ -14,6 +20,7 @@ class Counters extends Component {
             counter={counter}
             onIncrement={onIncrement}
             onDelete={onDelete}
+            onDecrement={onDecrement}
           ></Counter>
         ))}
       </div>
